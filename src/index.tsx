4eb1de4dade64faed263d796c3380cea5e79421c
Drakonis96 <41027003+Drakonis96@users.noreply.github.com>
2025-06-06 00:43:17 +0200

    Add PWA support

--- src/index.tsx
+++ src/index.tsx
@@ -13,3 +13,11 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register(`${process.env.PUBLIC_URL}/service-worker.js`).catch(() => {
+      console.warn('Service worker registration failed');
+    });
+  });
+}
